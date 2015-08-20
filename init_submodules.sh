58965a1925e550a3fcd989084ed1e8576ebd00d3
absala <info@absala.de>
2015-08-21 01:31:10 +0200

    taskwiki

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
 set -e
-
 git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
 while read path_key path
 do
+set +e
+echo _adding submodule $path _
 url_key=$(echo $path_key | sed 's/\.path/.url/')
+branch_key=$(echo $path_key | sed 's/\.path/.branch/')
 url=$(git config -f .gitmodules --get "$url_key")
-git submodule add $url $path
+branch=$(git config -f .gitmodules --get "$branch_key")
+
+if [ "$branch" = "" ]
+then
+    branch="master"
+fi
+echo url $url
+echo path $path
+echo branch $branch
+set -e
+git submodule add $url $path -b $branch
 done
